64d446adc8535ce37ba88c7b22197060c8f19821
Harsh Krishna <87293901+harshkrishna17@users.noreply.github.com>
2022-01-29 13:21:36 +0530

    Update get_all_scouting.R

--- get_all_scouting.R
+++ get_all_scouting.R
@@ -1,21 +1,35 @@
-# Libraries 
+# Libraries
 
 library(worldfootballR)
+library(purrr)
 
-# Function
+# Collect all team links 
 
-vector <- c("https://fbref.com/en/players/f586779e/Tammy-Abraham", "https://fbref.com/en/players/59e6e5bf/Dominic-Calvert-Lewin") 
+pl <- fb_teams_urls("https://fbref.com/en/comps/9/Premier-League-Stats")
+seriea <- fb_teams_urls("https://fbref.com/en/comps/11/Serie-A-Stats")
+ligue1 <- fb_teams_urls("https://fbref.com/en/comps/13/Ligue-1-Stats")
+bundesliga <- fb_teams_urls("https://fbref.com/en/comps/20/Bundesliga-Stats")
+laliga <- fb_teams_urls("https://fbref.com/en/comps/12/La-Liga-Stats")
+
+teams <- c(pl, seriea, ligue1, bundesliga, laliga)
+
+# Collect player links from team links
+
+players <- teams %>% 
+  purrr::map(fb_player_urls)
+
+# Convert list to vector
+
+players <- purrr::flatten_chr(players)
+
+# Function to extract player scouting reports from player links. 
+# If FBref doesn't contain the data for a specific player, it will skip through that particular player. 
 
 get_all_scouting <- function(vec) {
-  
-  df <- data.frame(matrix(NA, nrow = 745, ncol = 8))
-  
-  for(i in 1:length(vec)) {
-    
-    df[, ] <- fb_player_scouting_report(vec[i], pos_versus = "primary")
-  }
-  
-  return(df)
+  f_possibly <- purrr::possibly(fb_player_scouting_report, otherwise = data.frame())
+  purrr::map_dfr(vec, f_possibly)
 }
 
-df <- get_all_scouting(vec = vector)
+# Apply the function
+
+df <- get_all_scouting(vec = players)
